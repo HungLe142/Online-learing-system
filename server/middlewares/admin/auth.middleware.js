9bda1e6ed62d1d8785891ed95e793e61b008e3b9
VietKhiem12a5 <khiem.dinhnguyenviet@hcmut.edu.vn>
2024-12-13 22:56:47 +0700

    kiểm soát token admin

--- server/middlewares/admin/auth.middleware.js
+++ server/middlewares/admin/auth.middleware.js
@@ -1,14 +1,23 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(403).send("Không có quyền truy cập!");
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(401).send("Token không hợp lệ!");
+exports.authenticateToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).json({ error: "Vui lòng đăng nhập." });
   }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Token không hợp lệ." });
+    }
+    req.user = user; 
+    next();
+  });
+};
+
+exports.checkAdmin = (req, res, next) => {
+  if (req.user.quyen !== "admin") {
+    return res.status(403).json({ error: "Bạn không có quyền truy cập." });
+  }
+  next();
 };
