d9a7a3fba22ee96c1658c94e79e68c65479285ce
Hung Le <lethaihung142@gmail.com>
2024-12-17 00:31:07 +0700

    change API call, suit with new backend

--- client/src/modules/St_course/services/course_data_process.js
+++ client/src/modules/St_course/services/course_data_process.js
@@ -13,15 +13,15 @@ const courseDataProcess = async (studentId) => {
     const instructorImage = "https://cdn.builder.io/api/v1/image/assets/TEMP/f74733f6d18a33e68478744ebca07adcdd1065684e56984388baba9a3cee1c38?placeholderIfAbsent=true&apiKey=673b43bfd43741dfb5fb4f80631ec9b7";
 
     // Kiểm tra nếu apiData là một mảng
-    if (!Array.isArray(apiData)) {
-      throw new TypeError('API data is not an array');
-    }
-
-    const processedData = apiData.map(item => ({
+    // if (!Array.isArray(apiData)) {
+    //   throw new TypeError('API data is not an array');
+    // }
+    let data = apiData.message
+    const processedData = data.map(item => ({
       imageUrl: imageUrl,
-      title: item.Lop.ten_lop,
+      title: item.ten_lop,
       instructorImage: instructorImage,
-      instructorName: item.Lop.GiangVien.User.ho_ten,
+      instructorName: item.GiangVien.User.ho_ten,
       currentLesson: 0, // Bạn có thể cập nhật giá trị này nếu có thông tin
       totalLessons: 0 // Bạn có thể cập nhật giá trị này nếu có thông tin
     }));
